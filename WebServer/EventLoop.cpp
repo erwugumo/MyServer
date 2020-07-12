009613570ebab00c2574432e8f5d2d6fe5b6849a
erwugumo <erwugumo@163.com>
2020-07-12 22:52:23 +0800

    analyse EventLoop

--- WebServer/EventLoop.cpp
+++ WebServer/EventLoop.cpp
@@ -5,15 +5,42 @@
 #include <sys/epoll.h>
 #include <iostream>
 using namespace std;
-
+/*
+__thread是GCC内置的线程局部存储设施，存取效率可以和全局变量相比。
+__thread变量每一个线程有一份独立实体，各个线程的值互不干扰。
+可以用来修饰那些带有全局性且值可能变，但是又不值得用全局变量保护的变量。
+__thread使用规则：只能修饰POD类型(类似整型指针的标量，不带自定义的构造、拷贝、赋值、析构的类型，二进制内容可以任意复制
+memset,memcpy,且内容可以复原)，不能修饰class类型，因为无法自动调用构造函数和析构函数，可以用于修饰全局变量，
+函数内的静态变量，不能修饰函数的局部变量或者class的普通成员变量，且__thread变量值只能初始化为编译器常量
+(值在编译器就可以确定const int i=5,运行期常量是运行初始化后不再改变const int i=rand()).
+如果希望修饰带自定义构造和析构函数的类，需要用到指针。
+*/
 __thread EventLoop* t_loopInThisThread = 0;
 
 int createEventfd()
 {
+    /*
+    eventfd 是 Linux 的一个系统调用，创建一个文件描述符用于事件通知，自 Linux 2.6.22 以后开始支持。
+    eventfd() 创建一个 eventfd 对象，可以由用户空间应用程序实现事件等待/通知机制，或由内核通知用户空间应用程序事件。
+    该对象包含了由内核维护的无符号64位整数计数器 count 。使用参数 initval 初始化此计数器。
+    flags 可以是以下值的 OR 运算结果，用以改变 eventfd 的行为。
+
+    EFD_CLOEXEC (since Linux 2.6.27)
+    文件被设置成 O_CLOEXEC，创建子进程 (fork) 时不继承父进程的文件描述符。
+    EFD_NONBLOCK (since Linux 2.6.27)
+    文件被设置成 O_NONBLOCK，执行 read / write 操作时，不会阻塞。
+    EFD_SEMAPHORE (since Linux 2.6.30)
+    提供类似信号量语义的 read 操作，简单说就是计数值 count 递减 1。
+    */
     int evtfd = eventfd(0, EFD_NONBLOCK | EFD_CLOEXEC);
     if (evtfd < 0)
     {
         LOG << "Failed in eventfd";
+        /*
+        abort()
+        立即终止当前进程，产生异常程序终止
+        进程终止时不会销毁任何对象
+        */
         abort();
     }
     return evtfd;
@@ -28,6 +55,7 @@ EventLoop::EventLoop()
     callingPendingFunctors_(false),
     threadId_(CurrentThread::tid()),
     pwakeupChannel_(new Channel(this, wakeupFd_))
+    /*初始化列表好长*/
 {
     if (t_loopInThisThread)
     {
@@ -36,6 +64,7 @@ EventLoop::EventLoop()
     else
     {
         t_loopInThisThread = this;
+        /*this 是 C++ 中的一个关键字，也是一个 const 指针，它指向当前对象，通过它可以访问当前对象的所有成员。*/
     }
     //pwakeupChannel_->setEvents(EPOLLIN | EPOLLET | EPOLLONESHOT);
     pwakeupChannel_->setEvents(EPOLLIN | EPOLLET);
